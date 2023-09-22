244ba0d192db688daba9850368e168ca1069cd5a
Rambolink94 <knightsteele94@gmail.com>
2023-09-22 14:03:53 -0600

    Finished task 4 camp cleanup tasks

---
+++ CampCleanup.cs
@@ -0,0 +1,37 @@
+﻿using Godot;
+
+namespace LearningGodot;
+
+public partial class CampCleanup : Node2D
+{
+    public override void _Ready()
+    {
+        int totalContainments = 0;
+        foreach (string line in InputReader.ReadInput(4))
+        {
+            string[] sections = line.Split(',');
+
+            string[] leftRange = sections[0].Split('-');
+            string[] rightRange = sections[1].Split('-');
+
+            bool areContained = DoRangesOverlap(
+                int.Parse(leftRange[0]),
+                int.Parse(leftRange[1]),
+                int.Parse(rightRange[0]),
+                int.Parse(rightRange[1])
+            );
+
+            if (areContained)
+            {
+                totalContainments++;
+            }
+        }
+        
+        GD.Print(totalContainments);
+        
+        bool DoRangesOverlap(int leftLower, int leftUpper, int rightLower, int rightUpper)
+        {
+            return (leftLower <= rightUpper && leftUpper >= rightLower);
+        }
+    }
+}
