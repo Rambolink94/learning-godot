cfde55e1dd4ac88e0dc36c84f590a05edd6b43a6
Rambolink94 <knightsteele94@gmail.com>
2023-10-13 16:18:07 -0600

    Finished day 13 part 2...very nice!

--- DistressSignal.cs
+++ DistressSignal.cs
@@ -15,12 +15,37 @@ public partial class DistressSignal : PuzzleNode
         {
             if (line == string.Empty) continue;
             
+            var packet = ParsePacket(line);
+
+            pairs.Add(packet);
+        }
+        
+        // Add divider packets
+        var divider1 = ParsePacket("[[2]]");
+        var divider2 = ParsePacket("[[6]]");
+        
+        pairs.Add(divider1);
+        pairs.Add(divider2);
+        
+        pairs.Sort();
+
+        int startIndex = pairs.IndexOf(divider1);
+        int endIndex = pairs.IndexOf(divider2);
+
+        int decoderKey = (startIndex + 1) * (endIndex + 1);
+        
+        Print($"Decoder Key: {decoderKey}");
+
+        return;
+
+        Packet ParsePacket(string line)
+        {
             Packet root = null;
             Packet current = null;
             
             bool parsingValues = false;
             string currentValue = string.Empty;
-            
+
             foreach (char symbol in line)
             {
                 if (symbol == '[')
@@ -32,7 +57,7 @@ public partial class DistressSignal : PuzzleNode
                         current = root;
                         continue;
                     }
-                    
+
                     current = new Packet(current);
                 }
                 else if (symbol == ']')
@@ -41,7 +66,7 @@ public partial class DistressSignal : PuzzleNode
                     if (parsingValues)
                     {
                         parsingValues = false;
-                        
+
                         // Add last value and then assign values
                         if (int.TryParse(currentValue, out int parsedValue))
                         {
@@ -50,19 +75,19 @@ public partial class DistressSignal : PuzzleNode
 
                         currentValue = string.Empty;
                     }
-                    
+
                     current = current!.Parent;
                 }
                 else if (symbol == ',')
                 {
                     if (!parsingValues) continue;
-                    
+
                     // Compiling values
                     if (int.TryParse(currentValue, out int parsedValue))
                     {
                         current!.Add(parsedValue);
                     }
-                    
+
                     currentValue = string.Empty;
                 }
                 else
@@ -72,90 +97,12 @@ public partial class DistressSignal : PuzzleNode
                     currentValue += symbol;
                 }
             }
-            
-            pairs.Add(root);
+
+            return root;
         }
-
-        int sumOfIndices = 0;
-        pairs.Sort();
-        // Compare pairs
-        for (int i = 0, j = 1; i < pairs.Count; i += 2, j++)
-        {
-            var pair1 = pairs[i];
-            var pair2 = pairs[i + 1];
-
-            int order = ComparePackets(pair1, pair2);
-            if (order <= 0)
-            {
-                sumOfIndices += j;
-            }
-            
-            continue;
-
-            int ComparePackets(Packet a, Packet b, int index = 0)
-            {
-                int order = 0;
-                while (true)
-                {
-                    bool aHasValue = a.TryGetValue(index, out object value1);
-                    bool bHasValue = b.TryGetValue(index, out object value2);
-                    
-                    if (aHasValue && bHasValue)
-                    {
-                        if (value1 is int int1 && value2 is int int2)
-                        {
-                            // If both values are ints, than process them now.
-                            order = int1.CompareTo(int2);
-                        }
-                        else
-                        {
-                            // If not, at least one is a packet, so convert the one that isn't to a packet.
-                            if (value1 is not Packet packet1)
-                            {
-                                packet1 = new Packet(a);
-                                packet1.Add((int)value1);
-                            }
-
-                            if (value2 is not Packet packet2)
-                            {
-                                packet2 = new Packet(b);
-                                packet2.Add((int)value2);
-                            }
-
-                            order = ComparePackets(packet1, packet2);
-                        }
-                        
-                        index++;
-
-                        if (order != 0)
-                        {
-                            break;
-                        }
-
-                        continue;
-                    }
-
-                    if (!aHasValue && !bHasValue)
-                    {
-                        return 0;
-                    }
-
-                    if (!aHasValue)
-                    {
-                        return -1;
-                    }
-
-                    return 1;
-                }
-
-                return order;
-            }
-        }
-        
-        Print($"Sum of Indices: {sumOfIndices}");
     }
 
-    private class Packet : IComparer<Packet>
+    private class Packet : IComparable<Packet>, IComparer<Packet>
     {
         private readonly List<object> _data = new();
 
@@ -167,7 +114,7 @@ public partial class DistressSignal : PuzzleNode
         
         public Packet Parent { get; }
 
-        public bool TryGetValue(int i, out object value)
+        private bool TryGetValue(int i, out object value)
         {
             value = null;
             
@@ -211,13 +158,13 @@ public partial class DistressSignal : PuzzleNode
                         // If not, at least one is a packet, so convert the one that isn't to a packet.
                         if (value1 is not Packet packet1)
                         {
-                            packet1 = new Packet(a);
+                            packet1 = new Packet();
                             packet1.Add((int)value1);
                         }
 
                         if (value2 is not Packet packet2)
                         {
-                            packet2 = new Packet(b);
+                            packet2 = new Packet();
                             packet2.Add((int)value2);
                         }
 
@@ -248,6 +195,14 @@ public partial class DistressSignal : PuzzleNode
             }
 
             return order;
+        }
+
+        public int CompareTo(Packet other)
+        {
+            if (ReferenceEquals(this, other)) return 0;
+            if (ReferenceEquals(null, other)) return 1;
+            
+            return Compare(this, other);
         }
     }
 }
